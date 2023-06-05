04873e2de947f83ba1305221734fa82df15da6c0
Aerdelan <1874863790@qq.com>
2023-06-05 17:08:37 +0800

    hosk

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: Aerdelan 1874863790@qq.com
+ * @Date: 2023-06-01 17:27:33
+ * @LastEditors: Aerdelan 1874863790@qq.com
+ * @LastEditTime: 2023-06-05 16:19:15
+ * @FilePath: \v3-sgg-te\src\App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <h1>app根节点</h1>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Aerdelan 1874863790@qq.com
+ * @Date: 2023-06-01 17:27:33
+ * @LastEditors: Aerdelan 1874863790@qq.com
+ * @LastEditTime: 2023-06-03 15:13:09
+ * @FilePath: \v3-sgg-te\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
